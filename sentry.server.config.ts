1c6db72a53ed405676f1e93265fe9dd4a6196558
Yusup Maulana <yusup.maulana@kokatto.com>
2024-08-31 19:24:00 +0700

    feat: adding sentry monitor system

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://f44d6a769727861d59bb700cb2f23da2@o540435.ingest.us.sentry.io/4507871879561216",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
